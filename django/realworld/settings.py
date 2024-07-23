9b9e1f2ce6a6a9bd56bea6091fb4340a35999088
Ayush <ayushj0909@outlook.com>
2024-07-23 14:36:00 +0530

    Test

--- django/realworld/settings.py
+++ django/realworld/settings.py
@@ -14,11 +14,13 @@ from pathlib import Path
 from dotenv import load_dotenv
 import os
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 env_path = Path(__file__).resolve().parent.parent / '.env'
 load_dotenv(dotenv_path=env_path)
 
+print(os.getenv("DATABASE_NAME"))
 
 # Secret key should not be present in the configuration or the repository
 SECRET_KEY=os.getenv('SECRET_KEY')
