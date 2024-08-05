17aed8d7d42b278b66b1d500b9f8bcfc03d69711
Ayush <ayushj0909@outlook.com>
2024-08-05 15:39:37 +0530

    Fixed: Instance name, allocation size, subnetgroup subnet_ids

--- infrastructure/terraform/aws/db.tf
+++ infrastructure/terraform/aws/db.tf
@@ -1,17 +1,20 @@
 resource "aws_db_instance" "myDB" {
-  instance_class       = "db.t2.micro"
-  allocated_storage    = 5
+  instance_class       = "db.t3.micro"
+  allocated_storage    = 20
   storage_type         = "gp3"
   engine               = "mysql"
   engine_version       = "8.0.35"
   db_name              = "myDB"
-  identifier           = "mydatabse"
+  identifier           = "mydatabase" # Ensure this is unique
   username             = "admin"
-  password             = "123"
-  db_subnet_group_name = aws_db_subnet_group.DB_Subnet_Group.id
+  password             = "slime123" # Consider using AWS Secrets Manager
+  db_subnet_group_name = aws_db_subnet_group.DB_Subnet_Group.name
 }
 
 resource "aws_db_subnet_group" "DB_Subnet_Group" {
   name       = "my-db-subnet-group"
-  subnet_ids = [aws_subnet.main["ap-south-1a"].id]
+  subnet_ids = [
+    aws_subnet.main["ap-south-1a"].id,
+    aws_subnet.main["ap-south-1b"].id
+  ]
 }
