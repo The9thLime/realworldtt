c59d619d5deff5d91d5c37dc077d7ab9ea57bf96
Ayush <ayushj0909@outlook.com>
2024-08-16 21:19:13 +0530

    refactor: changed resource names

--- infrastructure/terraform/aws/vpc.tf
+++ infrastructure/terraform/aws/vpc.tf
@@ -1,28 +1,27 @@
-resource "aws_vpc" "main" {
-  cidr_block = "10.10.0.0/16"
+resource "aws_vpc" "realworld-vpc" {
+  cidr_block = "10.10.0.0/25"
 }
 
 locals {
   az_subnet_map = {
-    ap-south-1a = "10.10.1.0/24"
-    ap-south-1b = "10.10.2.0/24"
-    ap-south-1c = "10.10.3.0/24"
+    ap-south-1a = "10.10.0.0/27"
+    ap-south-1b = "10.10.0.32/27"
   }
 }
 
-resource "aws_subnet" "main" {
+resource "aws_subnet" "vpc" {
   for_each          = local.az_subnet_map
-  vpc_id            = aws_vpc.main.id
+  vpc_id            = aws_vpc.realworld-vpc.id
   availability_zone = each.key
   cidr_block        = each.value
 }
 
 resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.main.id
+  vpc_id = aws_vpc.realworld-vpc.id
 }
 
-resource "aws_route_table" "default" {
-  vpc_id = aws_vpc.main.id
+resource "aws_route_table" "realworld-rt" {
+  vpc_id = aws_vpc.realworld-vpc.id
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.igw.id
