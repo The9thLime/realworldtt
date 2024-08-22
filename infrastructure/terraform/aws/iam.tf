63be2f60289b8c9ef6b62e01ef0f1707b9b9e120
Ayush <ayushj0909@outlook.com>
2024-08-22 18:19:59 +0530

    feat: added node iam role

--- infrastructure/terraform/aws/iam.tf
+++ infrastructure/terraform/aws/iam.tf
@@ -16,6 +16,11 @@ resource "aws_iam_role" "eks-role" {
   name               = "eks-cluster-role"
 }
 
+resource "aws_iam_role" "eks-node-role" {
+  assume_role_policy = data.aws_iam_policy_document.eks-assume-role.json
+  name               = "eks-node-role"
+}
+
 resource "aws_iam_role_policy_attachment" "eks-role-AmazonEKSClusterPolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
   role       = aws_iam_role.eks-role.name
