b9f36a1c32de355a29625420aff17a288f5c55b2
Ayush <ayushj0909@outlook.com>
2024-09-07 21:15:56 +0530

    feat: helm_release.argocd.namespace now uses a variable

--- infrastructure/terraform/minikube/main.tf
+++ infrastructure/terraform/minikube/main.tf
@@ -1,8 +1,8 @@
 resource "helm_release" "argocd" {
-  chart = "argo-cd"
-  name  = "argocd"
-  repository = "https://argoproj.github.io/argo-helm"
-  namespace = "argocd"
+  chart            = "argo-cd"
+  name             = "argocd"
+  repository       = "https://argoproj.github.io/argo-helm"
+  namespace        = var.argocd_namespace
   create_namespace = true
   lifecycle {
     prevent_destroy = false
@@ -10,29 +10,13 @@ resource "helm_release" "argocd" {
 }
 
 locals {
-  argocd_deployed = helm_release.argocd.status == "deployed" ? 1:0
+  argocd_deployed = helm_release.argocd.status == "deployed" ? 1 : 0
 }
 
 resource "kubectl_manifest" "app-of-apps" {
-  count = local.argocd_deployed
+  count     = local.argocd_deployed
   yaml_body = file(var.parent_app_path)
 }
-
-# resource "null_resource" "sync_argocd_app" {
-#   count = local.argocd_deployed
-#
-#   provisioner "local-exec" {
-#     command = <<EOT
-#       kubectl port-forward svc/argocd-server -n argocd 8080:80 &
-#       sleep 10
-#       PASSWORD=$(kubectl get secrets/argocd-initial-admin-secret -n argocd -o jsonpath='{.data.password}' | base64 -d)
-#       argocd login localhost:8080 --username admin --password $PASSWORD --insecure
-#       argocd app sync app-of-apps
-#     EOT
-#   }
-#
-#   depends_on = [kubectl_manifest.app-of-apps]
-# }
 
 
 
